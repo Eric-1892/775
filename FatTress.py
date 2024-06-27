c450bac8a72909a66aa21be6579e0c9dfc6a1b5e
Eric-1892 <160651917+Eric-1892@users.noreply.github.com>
2024-06-26 18:00:53 -0700

    Update FatTress.py

--- FatTress.py
+++ FatTress.py
@@ -9,39 +9,37 @@ import os
 class FatTreeTopo(Topo):
     hostcount = 1
 
-    def build(self, k=4):
-        # Topology parameters
+    def __init__(self, k=4):
+        super(FatTreeTopo, self).__init__()
+        self.k = k
+        self.build(k)
+
+    def build(self, k):
         numPods = k
         numHostsPerEdgeSwitch = k // 2
         numEdgeSwitchesPerPod = k // 2
         numAggrSwitchesPerPod = k // 2
         numCoreSwitches = (k // 2) ** 2
         
-        # Add Core Switches
-        coreSwitches = [self.addSwitch('Core-S{}'.format(i + 1)) for i in range(numCoreSwitches)]
-
+        coreSwitches = [self.addSwitch('Core-S{}'.format(i + 1), dpid='{:016x}'.format(i + 1)) for i in range(numCoreSwitches)]
+        
         for p in range(1, numPods + 1):
-            # Add Aggregation and Edge Switches for each Pod
-            aggrSwitches = [self.addSwitch('Agg-S{}'.format((p-1)*2 + i + 1)) for i in range(numAggrSwitchesPerPod)]
-            edgeSwitches = [self.addSwitch('Acc-S{}'.format((p-1)*2 + i + 1)) for i in range(numEdgeSwitchesPerPod)]
-
-            # Link Aggregation Switches to Core Switches
+            aggrSwitches = [self.addSwitch('Agg-S{}'.format((p-1)*2 + i + 1), dpid='{:016x}'.format(numCoreSwitches + (p-1)*2 + i + 1)) for i in range(numAggrSwitchesPerPod)]
+            edgeSwitches = [self.addSwitch('Acc-S{}'.format((p-1)*2 + i + 1), dpid='{:016x}'.format(numCoreSwitches + numAggrSwitchesPerPod + (p-1)*2 + i + 1)) for i in range(numEdgeSwitchesPerPod)]
+            
             for i, aggr in enumerate(aggrSwitches):
                 for j, core in enumerate(coreSwitches):
                     if (j // numAggrSwitchesPerPod) % numAggrSwitchesPerPod == i:
                         self.addLink(aggr, core)
 
-            # Link Edge Switches to Aggregation Switches and Hosts
             for edge in edgeSwitches:
                 for aggr in aggrSwitches:
                     self.addLink(edge, aggr)
 
-                # Connecting hosts to edge switches
                 for _ in range(numHostsPerEdgeSwitch):
                     host = self.addHost('h{}'.format(FatTreeTopo.hostcount))
                     self.addLink(edge, host)
                     FatTreeTopo.hostcount += 1
-
 
 topos = {'FatTree': (lambda: FatTreeTopo())}
 
