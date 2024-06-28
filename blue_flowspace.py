a8d89060dc0bff50fb7e76189059fec4b6d217cb
Eric-1892 <160651917+Eric-1892@users.noreply.github.com>
2024-06-27 22:03:17 -0700

    Update blue_flowspace.py

--- blue_flowspace.py
+++ blue_flowspace.py
@@ -24,14 +24,16 @@ commands = [
     "fvctl -n add-flowspace blue18 0100000000000005 2 in_port=4,nw_src=10.0.0.12,nw_dst=10.0.0.10 blue=7",
 
     # Acc-S6
-    "fvctl -n add-flowspace blue19 0200000000000006 2 in_port=1,nw_src=10.0.0.9,nw_dst=10.0.0.11 blue=7",
-    "fvctl -n add-flowspace blue20 0200000000000006 2 in_port=1,nw_src=10.0.0.9,nw_dst=10.0.0.12 blue=7",
-    "fvctl -n add-flowspace blue21 0200000000000006 2 in_port=1,nw_src=10.0.0.10,nw_dst=10.0.0.11 blue=7",
-    "fvctl -n add-flowspace blue22 0200000000000006 2 in_port=1,nw_src=10.0.0.10,nw_dst=10.0.0.12 blue=7",
-    "fvctl -n add-flowspace blue23 0200000000000006 2 in_port=3,nw_src=10.0.0.11,nw_dst=10.0.0.9 blue=7",
-    "fvctl -n add-flowspace blue24 0200000000000006 2 in_port=3,nw_src=10.0.0.11,nw_dst=10.0.0.10 blue=7",
-    "fvctl -n add-flowspace blue25 0200000000000006 2 in_port=4,nw_src=10.0.0.12,nw_dst=10.0.0.9 blue=7",
-    "fvctl -n add-flowspace blue26 0200000000000006 2 in_port=4,nw_src=10.0.0.12,nw_dst=10.0.0.10 blue=7"
+    "fvctl -n add-flowspace blue6_1 0200000000000006 2 in_port=1,nw_src=10.0.0.9,nw_dst=10.0.0.11 blue=7",
+    "fvctl -n add-flowspace blue6_2 0200000000000006 2 in_port=1,nw_src=10.0.0.9,nw_dst=10.0.0.12 blue=7",
+    "fvctl -n add-flowspace blue6_3 0200000000000006 2 in_port=1,nw_src=10.0.0.10,nw_dst=10.0.0.11 blue=7",
+    "fvctl -n add-flowspace blue6_4 0200000000000006 2 in_port=1,nw_src=10.0.0.10,nw_dst=10.0.0.12 blue=7",
+    "fvctl -n add-flowspace blue7_1 0200000000000006 2 in_port=3,nw_src=10.0.0.11,nw_dst=10.0.0.9 blue=7",
+    "fvctl -n add-flowspace blue7_2 0200000000000006 2 in_port=3,nw_src=10.0.0.11,nw_dst=10.0.0.10 blue=7",
+    "fvctl -n add-flowspace blue7_3 0200000000000006 2 in_port=3,nw_src=10.0.0.11,nw_dst=10.0.0.12 blue=7",
+    "fvctl -n add-flowspace blue8_1 0200000000000006 2 in_port=4,nw_src=10.0.0.12,nw_dst=10.0.0.9 blue=7",
+    "fvctl -n add-flowspace blue8_2 0200000000000006 2 in_port=4,nw_src=10.0.0.12,nw_dst=10.0.0.10 blue=7",
+    "fvctl -n add-flowspace blue8_3 0200000000000006 2 in_port=4,nw_src=10.0.0.12,nw_dst=10.0.0.11 blue=7"
 ]
 
 for command in commands:
